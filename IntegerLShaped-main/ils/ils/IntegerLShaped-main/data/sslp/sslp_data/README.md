fc414fe8100f61aa063b305366e8d9f5d9bb24ae
PKeonW <133082629+PKeonW@users.noreply.github.com>
2025-09-07 16:04:55 +0900

    Delete IntegerLShaped-main directory

--- IntegerLShaped-main/ils/ils/IntegerLShaped-main/data/sslp/sslp_data/README.md
+++
@@ -1,1 +0,0 @@
-The SSLP SIPLIB instances should be downloaded from https://www2.isye.gatech.edu/~sahmed/siplib/sslp/sslp.html and unziped to this folder.
--- IntegerLShaped-main/ils/ils/IntegerLShaped-main/ils/instance_generators/instance_generator.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class InstanceGenerator(ABC):
-    @abstractmethod
-    def generate_instance(self):
-        """ Generates instance and stores as python dictionary. """
-        pass
